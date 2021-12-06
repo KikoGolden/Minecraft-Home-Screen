492e54534f172b66474ddb51511690102a4a4ba8
Kiko Golden <94949765+KikoGolden@users.noreply.github.com>
2021-12-06 17:43:54 +0200

    Add files via upload

---
+++ Minecraft/Minecraft home screen/myscript.js
@@ -0,0 +1,8 @@
+//--------------------tests
+let greeting = "Welcome here, "
+
+function greetPeter(greet){
+    console.log(`${greet}` + "Peter");
+}
+greetPeter(greeting);
+//--------------------main
